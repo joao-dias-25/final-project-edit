684c1e45f5bb2bff71ea8a6e70a65b9872979a34
anaferreira744 <78492007+anaferreira744@users.noreply.github.com>
2025-01-20 22:42:32 +0000

    criação dim_route, dim_date, atualização stg

--- dbt_ana/DIM_Lines.sql
+++ dbt_ana/DIM_Lines.sql
@@ -4,11 +4,12 @@ To reference this table onwards: {{ ref('your_model_name') }}
 
 {{
     config(
-        materialized = "view"  
+        materialized = "table"  
     )
 }}
 
-with lines as (
+with 
+lines as (
     select
         l.id as line_code,
         l.long_name,
@@ -20,15 +21,19 @@ with lines as (
 ),
 joined_data as (
     select
-        {{ 
-      dbt_utils.safe_cast('l.id', 'string') 
-  }} || '1970-01-01' AS pk_line,
+        {{ dbt_utils.generate_surrogate_key('l.id',  '1970-01-01')}} AS pk_line,
         l.line_code,
         l.long_name,
         l.short_name,
         l.color_line,
         l.lc,
-        r.line_type
+        r.line_type,
+        1 AS is_valid,
+        CURRENT_TIMESTAMP() AS inserted_at,
+        SESSION_USER()      AS inserted_by,
+        CURRENT_TIMESTAMP() AS updated_at,
+        SESSION_USER()      AS updated_by,
+        GENERATE_UUID()     AS uuid
     from lines l
     left join {{ ref('stg_routes') }} r
         on l.line_code = r.line_id
